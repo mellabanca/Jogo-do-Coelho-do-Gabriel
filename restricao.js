e76fb5f5a18d4e668dab065394ce09d73da4006d
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-05-31 17:11:25 -0300

    31/05/2022

---
+++ restricao.js
@@ -0,0 +1,15 @@
+class Restricao{
+    constructor(a,b){
+        var ultimaligacao=a.body.bodies.length-2;
+        this.ligacao=Constraint.create({
+            bodyA:a.body.bodies[ultimaligacao],
+            pointA:{x:0,y:0},
+            bodyB:b,
+            pointB:{x:0,y:0},
+            length:-10,
+            stiffness:0.01,
+
+        })
+        World.add(engine.world,this.ligacao);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -10,6 +10,16 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var chao;
+var corda;
+var fruta;
+var ligacao;
+var fundo, frutaImagem, coelho;
+
+function preload(){
+  fundo = loadImage("background.png");
+  frutaImagem = loadImage("melon.png");
+  coelho = loadImage("Rabbit-01.png");
+}
 
 function setup() 
 {
@@ -19,16 +29,26 @@ function setup()
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  imageMode(CENTER);
   textSize(50);
 
   chao = new Chao(200, 690, 600, 20);
+ corda= new Rope(6,{x:245,y:30});
+ fruta=Bodies.circle(300,300,15);
+ Matter.Composite.add(corda.body,fruta);
+ligacao= new Restricao(corda,fruta);
+
 }
 
 function draw() 
 {
   background(51);
+  image(fundo, width/2, height/2, 500, 700);
+
   Engine.update(engine);
   chao.mostrar();
+  corda.show();
+  image(frutaImagem, fruta.position.x,fruta.position.y,60,60);
 }
 
 
