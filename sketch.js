e3ecae4141566f127a76024fbfbeba5a641e8c75
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-06-23 17:29:54 -0300

    23/06/2022

--- sketch.js
+++ sketch.js
@@ -18,6 +18,16 @@ var spritecoelho
 var botao;
 var piscando, comendo,triste;
 var musicaFundo, musica, perder, ganhar, musicaVento;
+var balao; 
+var mute;
+var botao2;
+var botao3;
+var corda2;
+var corda3;
+var ligacao2;
+var ligacao3;
+
+
 
 function preload(){
   fundo = loadImage("background.png");
@@ -41,7 +51,21 @@ function preload(){
 
 function setup() 
 {
-  createCanvas(500,700);
+  var estaNoCelular = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if(estaNoCelular){
+    canW = displayWidth;
+    canH = displayHeight;
+    createCanvas(displayWidth+80,displayHeight);
+  } else {
+    canW = windowWidth;
+    canH = windowHeight;
+    createCanvas(windowWidth,windowHeight);
+  }
+
+ 
+  musicaFundo.play();
+  musicaFundo.setVolume(0.3);
   engine = Engine.create();
   world = engine.world;
 
@@ -54,13 +78,19 @@ function setup()
   imageMode(CENTER);
   textSize(50);
 
-  chao = new Chao(200, 690, 600, 20);
- corda= new Rope(6,{x:245,y:30});
+  chao = new Chao(200, canH, 600, 20);
+ corda= new Rope(8,{x:40,y:30});
+ corda2= new Rope(7,{x:370,y:40});
+ corda3= new Rope(4,{x:400,y:225});
+
+
  fruta=Bodies.circle(300,300,15);
  Matter.Composite.add(corda.body,fruta);
 ligacao= new Restricao(corda,fruta);
+ligacao2= new Restricao(corda2,fruta);
+ligacao3= new Restricao(corda3,fruta);
 
-spritecoelho=createSprite(250,630,100,100);
+spritecoelho=createSprite(170,canH-80,100,100);
 spritecoelho.addImage(coelho);
 spritecoelho.scale=0.2;
 spritecoelho.addAnimation("piscando", piscando);
@@ -69,33 +99,56 @@ spritecoelho.addAnimation("triste",triste);
 spritecoelho.changeAnimation("piscando");
 
 botao=createImg('cut_btn.png');
-botao.position(220,30);
+botao.position(20,30);
 botao.size(50,50);
 botao.mouseClicked(cortar);
+
+botao2=createImg('cut_btn.png');
+botao2.position(330,35);
+botao2.size(50,50);
+botao2.mouseClicked(cortar2);
+
+botao3=createImg('cut_btn.png');
+botao3.position(360,200);
+botao3.size(50,50);
+botao3.mouseClicked(cortar3);
+
+
+mute=createImg('mute.png');
+mute.position(450,20);
+mute.size(50,50);
+mute.mouseClicked(mudo);
 }
 
 function draw() 
 {
   
   background(51);
-  image(fundo, width/2, height/2, 500, 700);
+  image(fundo, width/2, height/2, displayWidth+80, displayHeight);
   
 
 
   Engine.update(engine);
   chao.mostrar();
+
+
   corda.show();
+  corda2.show();
+  corda3.show();
+
   if(fruta!==null){
     image(frutaImagem, fruta.position.x,fruta.position.y,60,60);
   }
   if(colisao(fruta,spritecoelho)===true){
     spritecoelho.changeAnimation("comendo");
+    ganhar.play();
   }
 
-if(fruta!==null&&fruta.position.y>=650){
+if(fruta!==null&&fruta.position.y>=height-70){
   spritecoelho.changeAnimation("triste");
  fruta= null;
-
+musicaFundo.stop();
+perder.play();
 }
 
   
@@ -107,6 +160,7 @@ function cortar(){
   corda.break()
   ligacao.separar();
   ligacao=null;
+  musica.play();
 }
 
 function colisao(corpo,sprite){
@@ -124,3 +178,32 @@ function colisao(corpo,sprite){
   }
 }
 
+function   vento(){
+  Matter.Body.applyForce(fruta,{x:0,y:0},{x:0.01,y:0});
+  musicaVento.play();
+}
+
+function mudo(){
+  if(musicaFundo.isPlaying()){
+    musicaFundo.stop();
+  }
+
+else{
+  musicaFundo.play();
+}
+}
+
+function cortar2(){
+  corda2.break()
+  ligacao2.separar();
+  ligacao2=null;
+  musica.play();
+
+}
+
+function cortar3(){
+  corda3.break()
+  ligacao3.separar();
+  ligacao3=null;
+  musica.play();
+}
